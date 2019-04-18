a1fa0d0fa1fcb64185b2591b172717fda31787cd
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-04-18 15:35:40 +0200

    refactor: update packages grar common + shaperon

--- src/ParcelRegistry.Api.CrabImport/CrabImport/IdempotentCommandHandlerModuleProcessor.cs
+++ src/ParcelRegistry.Api.CrabImport/CrabImport/IdempotentCommandHandlerModuleProcessor.cs
@@ -1,23 +1,23 @@
 namespace ParcelRegistry.Api.CrabImport.CrabImport
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Threading;
-    using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.AggregateSource;
     using Be.Vlaanderen.Basisregisters.CommandHandling;
-    using Be.Vlaanderen.Basisregisters.GrAr.Import.Api;
     using Be.Vlaanderen.Basisregisters.EventHandling;
+    using Be.Vlaanderen.Basisregisters.GrAr.Import.Api;
     using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
     using Parcel;
     using Parcel.Commands.Crab;
     using SqlStreamStore;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     public class IdempotentCommandHandlerModuleProcessor : IIdempotentCommandHandlerModuleProcessor
     {
         private readonly ConcurrentUnitOfWork _concurrentUnitOfWork;
         private readonly ParcelCommandHandlerModule _parcelCommandHandlerModule;
-        private readonly Func<IHasCrabProvenance, Parcel, Provenance> _provenanceFactory;
+        private readonly Func<IHasCrabProvenance, Parcel, Provenance> _provenanceFactory = new ParcelProvenanceFactory().CreateFrom;
         private readonly Func<IParcels> _getParcels;
 
         public IdempotentCommandHandlerModuleProcessor(
@@ -30,7 +30,6 @@ namespace ParcelRegistry.Api.CrabImport.CrabImport
         {
             _getParcels = getParcels;
             _concurrentUnitOfWork = concurrentUnitOfWork;
-            _provenanceFactory = provenanceFactory.CreateFrom;
 
             _parcelCommandHandlerModule = new ParcelCommandHandlerModule(
                 getParcels,
@@ -44,6 +43,7 @@ namespace ParcelRegistry.Api.CrabImport.CrabImport
         public async Task<CommandMessage> Process(
            dynamic commandToProcess,
            IDictionary<string, object> metadata,
+           int currentPosition,
            CancellationToken cancellationToken = default(CancellationToken))
         {
             switch (commandToProcess)
@@ -51,19 +51,19 @@ namespace ParcelRegistry.Api.CrabImport.CrabImport
                 case ImportTerrainObjectFromCrab command:
                     var commandTerrainObject = new CommandMessage<ImportTerrainObjectFromCrab>(command.CreateCommandId(), command, metadata);
                     await _parcelCommandHandlerModule.ImportTerrainObject(_getParcels, commandTerrainObject, cancellationToken);
-                    AddProvenancePipe.AddProvenance(() => _concurrentUnitOfWork, commandTerrainObject, _provenanceFactory);
+                    AddProvenancePipe.AddProvenance(() => _concurrentUnitOfWork, commandTerrainObject, _provenanceFactory, currentPosition);
                     return commandTerrainObject;
 
                 case ImportTerrainObjectHouseNumberFromCrab command:
                     var commandTerrainObjectHouseNumber = new CommandMessage<ImportTerrainObjectHouseNumberFromCrab>(command.CreateCommandId(), command, metadata);
                     await _parcelCommandHandlerModule.ImportTerrainObjectHouseNumber(_getParcels, commandTerrainObjectHouseNumber, cancellationToken);
-                    AddProvenancePipe.AddProvenance(() => _concurrentUnitOfWork, commandTerrainObjectHouseNumber, _provenanceFactory);
+                    AddProvenancePipe.AddProvenance(() => _concurrentUnitOfWork, commandTerrainObjectHouseNumber, _provenanceFactory, currentPosition);
                     return commandTerrainObjectHouseNumber;
 
                 case ImportSubaddressFromCrab command:
                     var commandSubaddress = new CommandMessage<ImportSubaddressFromCrab>(command.CreateCommandId(), command, metadata);
                     await _parcelCommandHandlerModule.ImportSubaddress(_getParcels, commandSubaddress, cancellationToken);
-                    AddProvenancePipe.AddProvenance(() => _concurrentUnitOfWork, commandSubaddress, _provenanceFactory);
+                    AddProvenancePipe.AddProvenance(() => _concurrentUnitOfWork, commandSubaddress, _provenanceFactory, currentPosition);
                     return commandSubaddress;
 
                 default:
