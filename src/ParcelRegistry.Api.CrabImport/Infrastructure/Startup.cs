03382ebcd168ddb5d74f60381864c8db2d7cf7ec
David Cumps <david@cumps.be>
2019-08-26 13:27:06 +0200

    feat: bump to .net 2.2.6

--- src/ParcelRegistry.Api.CrabImport/Infrastructure/Startup.cs
+++ src/ParcelRegistry.Api.CrabImport/Infrastructure/Startup.cs
@@ -112,12 +112,24 @@ namespace ParcelRegistry.Api.CrabImport.Infrastructure
             StartupHelpers.EnsureSqlStreamStoreSchema<Startup>(streamStore, loggerFactory);
 
             app
-                .UseDatadog<Startup>(
-                    serviceProvider,
-                    loggerFactory,
-                    datadogToggle,
-                    debugDataDogToggle,
-                    _configuration["DataDog:ServiceName"])
+                .UseDataDog<Startup>(new DataDogOptions
+                {
+                    Common =
+                    {
+                        ServiceProvider = serviceProvider,
+                        LoggerFactory = loggerFactory
+                    },
+                    Toggles =
+                    {
+                        Enable = datadogToggle,
+                        Debug = debugDataDogToggle
+                    },
+                    Tracing =
+                    {
+                        ServiceName = _configuration["DataDog:ServiceName"],
+                    }
+                })
+
                 .UseDefaultForApi(new StartupUseOptions
                 {
                     Common =
