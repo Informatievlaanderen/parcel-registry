6911862abd4275b8ae9c80d28bb270eb28ec564e
David Cumps <david@cumps.be>
2020-01-23 08:26:29 +0100

    feat: use distributed lock for syndication

--- src/ParcelRegistry.Projections.Syndication/Program.cs
+++ src/ParcelRegistry.Projections.Syndication/Program.cs
@@ -8,6 +8,7 @@ namespace ParcelRegistry.Projections.Syndication
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
+    using Be.Vlaanderen.Basisregisters.Aws.DistributedMutex;
     using Modules;
     using Serilog;
     using System;
@@ -48,18 +49,24 @@ namespace ParcelRegistry.Projections.Syndication
             var container = ConfigureServices(configuration);
 
             Log.Information("Starting BuildingRegistry.Projections.Syndication");
-            
+
             try
             {
-                await MigrationsHelper.RunAsync(
-                    configuration.GetConnectionString("SyndicationProjectionsAdmin"),
-                    container.GetService<ILoggerFactory>(),
-                    ct);
+                DistributedLock<Program>.Run(
+                    async () =>
+                    {
+                        await MigrationsHelper.RunAsync(
+                            configuration.GetConnectionString("SyndicationProjectionsAdmin"),
+                            container.GetService<ILoggerFactory>(),
+                            ct);
 
-                await Task.WhenAll(StartRunners(configuration, container, ct));
+                        await Task.WhenAll(StartRunners(configuration, container, ct));
 
-                Log.Information("Running... Press CTRL + C to exit.");
-                Closing.WaitOne();
+                        Log.Information("Running... Press CTRL + C to exit.");
+                        Closing.WaitOne();
+                    },
+                    DistributedLockOptions.LoadFromConfiguration(configuration) ?? DistributedLockOptions.Defaults,
+                    container.GetService<ILogger<Program>>());
             }
             catch (Exception e)
             {
