d61561f607fa818a4e9786f096c19ebcc4b8b46b
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-02-14 12:32:20 +0100

    fix: add index to list

---
+++ src/ParcelRegistry.Projections.Legacy/Migrations/20200214112716_AddIndexOnList.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParcelRegistry.Projections.Legacy.Migrations
+{
+    public partial class AddIndexOnList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ParcelDetails_Complete_Removed",
+                schema: "ParcelRegistryLegacy",
+                table: "ParcelDetails",
+                columns: new[] { "Complete", "Removed" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ParcelDetails_Complete_Removed",
+                schema: "ParcelRegistryLegacy",
+                table: "ParcelDetails");
+        }
+    }
+}
