82b4335cf919c00a69aa2dbdddccc7d113b1cdc1
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-11-09 10:36:48 +0100

    chore: add processed to archive for fix to staging/prd + redoimporter

---
+++ test/ParcelRegistry.Console.RedoParcelImport/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("ParcelRegistry Console Redo Parcel Import")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4fdbc151-9893-43a2-8249-0ce45b88e369")]
