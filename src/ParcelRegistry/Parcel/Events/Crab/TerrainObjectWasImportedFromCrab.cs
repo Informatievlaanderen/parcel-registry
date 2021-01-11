cc2f2801c67548df32db83131c75aca78ac72b1c
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-11 12:18:13 +0100

    docs: added EventDescription/EventPropertyDescriptions GRAR-1619

--- src/ParcelRegistry/Parcel/Events/Crab/TerrainObjectWasImportedFromCrab.cs
+++ src/ParcelRegistry/Parcel/Events/Crab/TerrainObjectWasImportedFromCrab.cs
@@ -6,19 +6,43 @@ namespace ParcelRegistry.Parcel.Events.Crab
     using NodaTime;
 
     [EventName("CRAB-TerrainObjectWasImported")]
+    [EventDescription("Legacy event om tblterreinobject en tblterreinobject_hist te importeren voor kadPercelen.")]
     public class TerrainObjectWasImportedFromCrab
     {
+        [EventPropertyDescription("CRAB-identificator van het terreinobject.")]
         public int TerrainObjectId { get; }
+        
+        [EventPropertyDescription("GRB-identificator van het terreinobject.")]
         public string IdentifierTerrainObject { get; }
+        
+        [EventPropertyDescription("Aard van het terreinobject.")]
         public string TerrainObjectNatureCode { get; }
+        
+        [EventPropertyDescription("X-coördinaat van de centroïde van het terreinobject.")]
         public decimal? XCoordinate { get; }
+        
+        [EventPropertyDescription("Y-coördinaat van de centroïde van het terreinobject.")]
         public decimal? YCoordinate { get; }
+        
+        [EventPropertyDescription("Aard van het gebouw.")]
         public string BuildingNature { get; }
+        
+        [EventPropertyDescription("Datum waarop het object is ontstaan in werkelijkheid.")]
         public LocalDateTime? Begin { get; }
+        
+        [EventPropertyDescription("Datum waarop het object in werkelijkheid ophoudt te bestaan.")]
         public LocalDateTime? End { get; }
+        
+        [EventPropertyDescription("Tijdstip waarop het object werd ingevoerd in de databank.")]
         public Instant Timestamp { get; }
+        
+        [EventPropertyDescription("Operator door wie het object werd ingevoerd in de databank.")]
         public string Operator { get; }
+        
+        [EventPropertyDescription("Bewerking waarmee het object werd ingevoerd in de databank.")] 
         public CrabModification? Modification { get; }
+        
+        [EventPropertyDescription("Organisatie die het object heeft ingevoerd in de databank.")]
         public CrabOrganisation? Organisation { get; }
 
         public TerrainObjectWasImportedFromCrab(
