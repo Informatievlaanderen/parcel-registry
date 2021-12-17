a2863d060da0518601471d14e2edece497f6e6c5
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2021-12-17 13:19:25 +0100

    fix: use async startup of projections to fix hanging migrations

--- src/ParcelRegistry.Projector/Infrastructure/Startup.cs
+++ src/ParcelRegistry.Projector/Infrastructure/Startup.cs
@@ -188,7 +188,7 @@ namespace ParcelRegistry.Projector.Infrastructure
                     }
                 })
 
-                .UseProjectionsManager(new ProjectionsManagerOptions
+                .UseProjectionsManagerAsync(new ProjectionsManagerOptions
                 {
                     Common =
                     {
