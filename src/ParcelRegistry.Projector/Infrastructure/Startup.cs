810d3459713302107a107f594d65fbfd37895341
Patrick Gallik <patrickgallik@gmail.com>
2022-02-25 14:28:34 +0100

    feat: update api to 17.0.0

--- src/ParcelRegistry.Projector/Infrastructure/Startup.cs
+++ src/ParcelRegistry.Projector/Infrastructure/Startup.cs
@@ -135,7 +135,7 @@ namespace ParcelRegistry.Projector.Infrastructure
             ApiDebugDataDogToggle debugDataDogToggle,
             HealthCheckService healthCheckService)
         {
-            StartupHelpers.CheckDatabases(healthCheckService, DatabaseTag).GetAwaiter().GetResult();
+            StartupHelpers.CheckDatabases(healthCheckService, DatabaseTag, loggerFactory).GetAwaiter().GetResult();
 
             app
                 .UseDataDog<Startup>(new DataDogOptions
