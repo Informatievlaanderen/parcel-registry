7222aa2dfb0da6ac1babd1b048e2ba5522ab8d22
Arne Dumarey <dumarey.arne@gmail.com>
2022-04-04 13:23:03 +0200

    fix: set oslo context type to string GAWR-2931

--- src/ParcelRegistry.Api.Oslo/Parcel/Responses/ParcelListOsloResponse.cs
+++ src/ParcelRegistry.Api.Oslo/Parcel/Responses/ParcelListOsloResponse.cs
@@ -6,7 +6,6 @@ namespace ParcelRegistry.Api.Oslo.Parcel.Responses
     using Be.Vlaanderen.Basisregisters.GrAr.Common;
     using Be.Vlaanderen.Basisregisters.GrAr.Legacy;
     using Be.Vlaanderen.Basisregisters.GrAr.Legacy.Perceel;
-    using Be.Vlaanderen.Basisregisters.Api.JsonConverters;
     using Infrastructure.Options;
     using Microsoft.Extensions.Options;
     using Newtonsoft.Json;
@@ -20,8 +19,7 @@ namespace ParcelRegistry.Api.Oslo.Parcel.Responses
         /// </summary>
         [DataMember(Name = "@context", Order = 0)]
         [JsonProperty(Required = Required.DisallowNull)]
-        [JsonConverter(typeof(PlainStringJsonConverter))]
-        public object Context { get; set; }
+        public string Context { get; set; }
 
         /// <summary>
         /// De verzameling van percelen.
