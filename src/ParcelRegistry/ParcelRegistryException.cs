e120b54d2054e3db076fa5016c7d7afd486bf4f3
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-08-01 17:06:38 +0200

    fix: conform to Serializable

--- src/ParcelRegistry/ParcelRegistryException.cs
+++ src/ParcelRegistry/ParcelRegistryException.cs
@@ -1,14 +1,24 @@
 namespace ParcelRegistry
 {
     using System;
+    using System.Runtime.Serialization;
     using Be.Vlaanderen.Basisregisters.AggregateSource;
 
     public abstract class ParcelRegistryException : DomainException
     {
-        protected ParcelRegistryException() { }
+        protected ParcelRegistryException()
+        { }
 
-        protected ParcelRegistryException(string message) : base(message) { }
+        protected ParcelRegistryException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
 
-        protected ParcelRegistryException(string message, Exception inner) : base(message, inner) { }
+        protected ParcelRegistryException(string message)
+            : base(message)
+        { }
+
+        protected ParcelRegistryException(string message, Exception inner)
+            : base(message, inner)
+        { }
     }
 }
