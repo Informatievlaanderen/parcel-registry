0305e0d4ef06e21aba4a61c2b1d62fb34526cc66
pgallik <patrickgallik@gmail.com>
2022-11-21 14:27:41 +0100

    feat: add backoffice proj

---
+++ src/ParcelRegistry.Api.BackOffice.Abstractions/Migrations/20221121132548_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelRegistry.Api.BackOffice.Abstractions.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ParcelRegistryBackOffice");
+
+            migrationBuilder.CreateTable(
+                name: "ParcelAddressRelation",
+                schema: "ParcelRegistryBackOffice",
+                columns: table => new
+                {
+                    AddressPersistentLocalId = table.Column<int>(type: "int", nullable: false),
+                    ParcelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParcelAddressRelation", x => x.AddressPersistentLocalId)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParcelAddressRelation",
+                schema: "ParcelRegistryBackOffice");
+        }
+    }
+}
