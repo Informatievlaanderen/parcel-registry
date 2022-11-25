c461e23f3e5b419dbba73e7b581f76fdd28055b7
emalfroy <egon.malfroy@vlaanderen.be>
2022-11-25 16:26:28 +0100

    fix: address consumer

--- src/ParcelRegistry.Consumer.Address/Projections/AddressKafkaProjection.cs
+++ src/ParcelRegistry.Consumer.Address/Projections/AddressKafkaProjection.cs
@@ -1,24 +1,56 @@
 namespace ParcelRegistry.Consumer.Address.Projections
 {
     using System;
+    using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.EntityFrameworkCore;
 
     public class AddressKafkaProjection : ConnectedProjection<ConsumerAddressContext>
     {
+        private async Task CatchDbUpdateException(Func<Task> func, ConsumerAddressContext context)
+        {
+            try
+            {
+                await func();
+            }
+            catch (DbUpdateException ex)
+            {
+                const int uniqueConstraintExceptionCode = 2627;
+                const int uniqueIndexExceptionCode = 2601;
+
+                if (ex.InnerException is SqlException innerException
+                    && innerException.Number is uniqueConstraintExceptionCode or uniqueIndexExceptionCode)
+                {
+                    // When the service crashes between EF ctx saveChanges and Kafka's commit offset
+                    // it will try to reconsume the same message that was already saved to db causing duplicate key exception.
+                    // In that case ignore.
+                    context.ChangeTracker.Clear();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
         public AddressKafkaProjection()
         {
             When<AddressWasMigratedToStreetName>(async (context, message, ct) =>
             {
-                await context
-                    .AddressConsumerItems
-                    .AddAsync(new AddressConsumerItem(
-                            message.AddressPersistentLocalId,
-                            Guid.Parse(message.AddressId),
-                            AddressStatus.Parse(message.Status),
-                            message.IsRemoved)
-                        , ct);
-                await context.SaveChangesAsync(ct);
+                await CatchDbUpdateException(async () =>
+                {
+                    await context
+                        .AddressConsumerItems
+                        .AddAsync(new AddressConsumerItem(
+                                message.AddressPersistentLocalId,
+                                Guid.Parse(message.AddressId),
+                                AddressStatus.Parse(message.Status),
+                                message.IsRemoved)
+                            , ct);
+                    await context.SaveChangesAsync(ct);
+                }, context);
             });
 
             When<AddressWasProposedV2>(async (context, message, ct) =>
