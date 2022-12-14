4c29f07a5c498ae86da3ea0f7d05c2a86e302194
emalfroy <egon.malfroy@vlaanderen.be>
2022-12-14 08:53:23 +0100

    fix: address id documentation

--- src/ParcelRegistry.Api.BackOffice.Abstractions/Requests/AttachAddressRequest.cs
+++ src/ParcelRegistry.Api.BackOffice.Abstractions/Requests/AttachAddressRequest.cs
@@ -8,7 +8,7 @@ namespace ParcelRegistry.Api.BackOffice.Abstractions.Requests
     public class AttachAddressRequest
     {
         /// <summary>
-        /// Adres welke dient gekoppeld te worden aan het perceel.
+        /// De unieke en persistente identificator van het adres.
         /// </summary>
         [DataMember(Name = "AdresId", Order = 0)]
         [JsonProperty(Required = Required.Always)]
--- src/ParcelRegistry.Api.BackOffice.Abstractions/Requests/DetachAddressRequest.cs
+++ src/ParcelRegistry.Api.BackOffice.Abstractions/Requests/DetachAddressRequest.cs
@@ -8,7 +8,7 @@ namespace ParcelRegistry.Api.BackOffice.Abstractions.Requests
     public class DetachAddressRequest
     {
         /// <summary>
-        /// Adres welke dient ontkoppeld te worden van het perceel.
+        /// De unieke en persistente identificator van het adres.
         /// </summary>
         [DataMember(Name = "AdresId", Order = 0)]
         [JsonProperty(Required = Required.Always)]
