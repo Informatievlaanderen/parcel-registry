68e1ac0f0538759a5787318a775852cc0c84746b
pgallik <patrickgallik@gmail.com>
2023-01-20 11:15:15 +0100

    feat: add acmidm  integrationtests

--- build.fsx
+++ build.fsx
@@ -46,7 +46,11 @@ Target.create "Build_Solution" (fun _ ->
   buildSolution "ParcelRegistry"
 )
 
-Target.create "Test_Solution" (fun _ -> test "ParcelRegistry")
+Target.create "Test_Solution" (fun _ ->
+    [
+        "test" @@ "ParcelRegistry.Tests"
+    ] |> List.iter testWithDotNet
+)
 
 Target.create "Publish_Solution" (fun _ ->
   [
