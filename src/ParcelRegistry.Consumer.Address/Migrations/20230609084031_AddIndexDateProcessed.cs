7c417747e0157e1fcbed18bb1ac1512de558c9f7
emalfroy <egon.malfroy@gmail.com>
2023-06-09 10:41:01 +0200

    feat: add consumer controller

---
+++ src/ParcelRegistry.Consumer.Address/Migrations/20230609084031_AddIndexDateProcessed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelRegistry.Consumer.Address.Migrations
+{
+    public partial class AddIndexDateProcessed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcessedMessages_DateProcessed",
+                schema: "ParcelRegistryConsumerAddress",
+                table: "ProcessedMessages",
+                column: "DateProcessed");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProcessedMessages_DateProcessed",
+                schema: "ParcelRegistryConsumerAddress",
+                table: "ProcessedMessages");
+        }
+    }
+}
---
+++ src/ParcelRegistry.Projector/Consumer/ConsumerController.cs
@@ -0,0 +1,42 @@
+﻿namespace ParcelRegistry.Projector.Consumer
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api;
+    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Consumer;
+    using Dapper;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.Extensions.Configuration;
+    using ParcelRegistry.Consumer.Address;
+    using ParcelRegistry.Infrastructure;
+
+    [ApiVersion("1.0")]
+    [ApiRoute("consumers")]
+    public class ConsumersController : ApiController
+    {
+        private const string? ConsumerConnectionStringKey = "ConsumerAddress";
+
+        [HttpGet]
+        public async Task<IActionResult> Get(
+            [FromServices] IConfiguration configuration,
+            CancellationToken cancellationToken = default)
+        {
+            await using var sqlConnection =
+                new SqlConnection(configuration.GetConnectionString(ConsumerConnectionStringKey));
+            var result =
+                await sqlConnection.QueryFirstAsync<DateTimeOffset>(
+                    $"SELECT TOP(1) [{nameof(ProcessedMessage.DateProcessed)}] FROM [{Schema.ConsumerAddress}].[{ConsumerAddressContext.ProcessedMessageTable}] ORDER BY [{nameof(ProcessedMessage.DateProcessed)}] DESC");
+
+            return Ok(new[]
+            {
+                new
+                {
+                    Name = "Consumer van adres",
+                    LastProcessedMessage = result
+                }
+            });
+        }
+    }
+}
