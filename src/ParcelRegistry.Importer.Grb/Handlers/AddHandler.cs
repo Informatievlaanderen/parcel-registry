ae716fa1d093f56c36cdabe888c620e606ba34eb
pgallik <patrickgallik@gmail.com>
2023-07-11 13:18:21 +0200

    feat: importer implementation without fetching grb data

---
+++ src/ParcelRegistry.Importer.Grb/Handlers/AddHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace ParcelRegistry.Importer.Grb.Handlers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Infrastructure;
+    using MediatR;
+
+    public record GrbAddParcelRequest(GrbParcel GrbParcel) : GrbParcelRequest(GrbParcel);
+
+    public class AddHandler : IRequestHandler<GrbAddParcelRequest, ParcelResponse>
+    {
+        public Task<ParcelResponse> Handle(GrbAddParcelRequest request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
