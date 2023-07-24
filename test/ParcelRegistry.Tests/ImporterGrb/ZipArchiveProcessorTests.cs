588a9ed2dc5238c0d02643db60b5c5730028b7c3
pgallik <patrickgallik@gmail.com>
2023-07-24 11:14:41 +0200

    fix: zipArchiveProcessor read entry file path

---
+++ test/ParcelRegistry.Tests/ImporterGrb/ZipArchiveProcessorTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ParcelRegistry.Tests.ImporterGrb
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+    using FluentAssertions;
+    using Importer.Grb.Infrastructure.Download;
+    using Xunit;
+
+    public class ZipArchiveProcessorTests
+    {
+        [Fact]
+        public void GivenZipFile_ThenReadEntries()
+        {
+            using var zipFile = new FileStream($"{AppContext.BaseDirectory}/ImporterGrb/grb_download_file.zip", FileMode.Open, FileAccess.Read);
+            using var archive = new ZipArchive(zipFile, ZipArchiveMode.Read, false);
+
+            var result = new ZipArchiveProcessor().Open(archive);
+
+            result.Count.Should().Be(3);
+        }
+    }
+}
