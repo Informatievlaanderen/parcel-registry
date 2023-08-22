abe61dc157a7fcc3b004057989af4585719cad49
pgallik <patrickgallik@gmail.com>
2023-08-22 15:52:56 +0200

    fix: throw orderInvalidDateRangeException + notify slack of completed run

---
+++ src/ParcelRegistry.Importer.Grb/OrderInvalidDateRangeException.cs
@@ -0,0 +1,16 @@
+﻿namespace ParcelRegistry.Importer.Grb
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class OrderInvalidDateRangeException : Exception
+    {
+        public OrderInvalidDateRangeException(string message) : base(message)
+        { }
+
+        private OrderInvalidDateRangeException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
+    }
+}
