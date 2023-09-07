022693bbde8de217b016e6d340a7973517da942c
Arne Dumarey <dumarey.arne@gmail.com>
2023-09-07 22:27:29 +0200

    refactor: increase list performance by query model + index GAWR-5163

---
+++ src/ParcelRegistry.Api.Oslo/Parcel/List/ParcelListV2QueryItem.cs
@@ -0,0 +1,20 @@
+﻿namespace ParcelRegistry.Api.Oslo.Parcel.List
+{
+    using System;
+    using System.Collections.Generic;
+    using NodaTime;
+    using ParcelRegistry.Parcel;
+    using Projections.Legacy.ParcelDetailV2;
+
+    public sealed class ParcelListV2QueryItem
+    {
+        public string CaPaKey { get; init; }
+        public ParcelStatus Status => ParcelStatus.Parse(StatusAsString);
+
+        public string StatusAsString { get; init; }
+
+        public DateTimeOffset VersionTimestampAsDateTimeOffset { get; init; }
+
+        public Instant VersionTimestamp => Instant.FromDateTimeOffset(VersionTimestampAsDateTimeOffset);
+    }
+}
---
+++ src/ParcelRegistry.Projections.Legacy/Migrations/20230907202615_RemakeRemovedIndex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelRegistry.Projections.Legacy.Migrations
+{
+    public partial class RemakeRemovedIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ParcelDetailsV2_Removed",
+                schema: "ParcelRegistryLegacy",
+                table: "ParcelDetailsV2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParcelDetailsV2_Removed",
+                schema: "ParcelRegistryLegacy",
+                table: "ParcelDetailsV2",
+                column: "Removed")
+                .Annotation("SqlServer:Include", new[] { "CaPaKey", "Status", "VersionTimestamp" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ParcelDetailsV2_Removed",
+                schema: "ParcelRegistryLegacy",
+                table: "ParcelDetailsV2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParcelDetailsV2_Removed",
+                schema: "ParcelRegistryLegacy",
+                table: "ParcelDetailsV2",
+                column: "Removed");
+        }
+    }
+}
