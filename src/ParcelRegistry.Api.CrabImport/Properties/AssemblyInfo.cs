80e4967f74402a2ddec5e8f9c0a5ee990fd013f5
Arne Dumarey <dumarey.arne@gmail.com>
2023-12-05 14:59:22 +0100

    chore: remove crabimport + console bugfixes on legacy

--- src/ParcelRegistry.Api.CrabImport/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("ParcelRegistry CrabImport API")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("b02c5040-15ca-45ae-8423-82e02aa439b0")]
