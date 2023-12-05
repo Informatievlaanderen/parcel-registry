8e4d8c7a8af136e57d730621a9cdad669c5ec9d3
Arne Dumarey <dumarey.arne@gmail.com>
2023-12-05 14:59:22 +0100

    chore: remove crabimport + console bugfixes on legacy

--- test/ParcelRegistry.Console.RedoRemoveParcels/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("ParcelRegistry Console Redo Remove Parcels")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("0b71af03-a10e-4864-a8df-56bae12631a8")]
