3e20cfd9eed61ada1a64a98c4b53ff79e7f62f07
jvandaal <jonas.vandaal@outlook.be>
2024-04-29 11:55:28 +0200

    feat: add combined index isremoved and status on integration projections

---
+++ src/ParcelRegistry.Projections.Integration/Migrations/20240429095431_CombinedIndexIsRemovedStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelRegistry.Projections.Integration.Migrations
+{
+    /// <inheritdoc />
+    public partial class CombinedIndexIsRemovedStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_parcel_latest_items_is_removed_status",
+                schema: "integration_parcel",
+                table: "parcel_latest_items",
+                columns: new[] { "is_removed", "status" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_parcel_latest_items_is_removed_status",
+                schema: "integration_parcel",
+                table: "parcel_latest_items");
+        }
+    }
+}
