0980d423319b661c1371e45443d1bfbfbbdd9e70
jvandaal <jonas.vandaal@outlook.be>
2024-05-17 16:30:28 +0200

    fix: add legacy parcel address count ef migration

--- src/ParcelRegistry.Projections.Legacy/Migrations/20240517142953_AddCountOnParcelDetailAddress.Designer.cs
+++ src/ParcelRegistry.Projections.Legacy/Migrations/20240517142953_AddCountOnParcelDetailAddress.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ParcelRegistry.Projections.Legacy;
 
@@ -11,9 +12,11 @@ using ParcelRegistry.Projections.Legacy;
 namespace ParcelRegistry.Projections.Legacy.Migrations
 {
     [DbContext(typeof(LegacyContext))]
-    partial class LegacyContextModelSnapshot : ModelSnapshot
+    [Migration("20240517142953_AddCountOnParcelDetailAddress")]
+    partial class AddCountOnParcelDetailAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
