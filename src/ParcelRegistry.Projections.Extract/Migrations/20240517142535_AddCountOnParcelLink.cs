4e47e5c87c7dff6e03c76dc2e44fb46d80388c62
jvandaal <jonas.vandaal@outlook.be>
2024-05-17 16:26:54 +0200

    fix: add extract parcel link count ef migration

---
+++ src/ParcelRegistry.Projections.Extract/Migrations/20240517142535_AddCountOnParcelLink.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelRegistry.Projections.Extract.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountOnParcelLink : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "ParcelRegistryExtract",
+                table: "ProjectionStates");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "ParcelRegistryExtract",
+                table: "ProjectionStates",
+                column: "Name");
+
+            migrationBuilder.CreateTable(
+                name: "ParcelLinksWithCount",
+                schema: "ParcelRegistryExtract",
+                columns: table => new
+                {
+                    ParcelId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AddressPersistentLocalId = table.Column<int>(type: "int", nullable: false),
+                    CaPaKey = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false, defaultValue: 1),
+                    DbaseRecord = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParcelLinksWithCount", x => new { x.ParcelId, x.AddressPersistentLocalId })
+                        .Annotation("SqlServer:Clustered", false);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParcelLinksWithCount_AddressPersistentLocalId",
+                schema: "ParcelRegistryExtract",
+                table: "ParcelLinksWithCount",
+                column: "AddressPersistentLocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParcelLinksWithCount_CaPaKey",
+                schema: "ParcelRegistryExtract",
+                table: "ParcelLinksWithCount",
+                column: "CaPaKey")
+                .Annotation("SqlServer:Clustered", true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParcelLinksWithCount_ParcelId",
+                schema: "ParcelRegistryExtract",
+                table: "ParcelLinksWithCount",
+                column: "ParcelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParcelLinksWithCount",
+                schema: "ParcelRegistryExtract");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "ParcelRegistryExtract",
+                table: "ProjectionStates");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProjectionStates",
+                schema: "ParcelRegistryExtract",
+                table: "ProjectionStates",
+                column: "Name")
+                .Annotation("SqlServer:Clustered", true);
+        }
+    }
+}
