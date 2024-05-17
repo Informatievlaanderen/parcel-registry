2e4f8f2f7e4e582e7f5418bbc1431698c9f0de01
jvandaal <jonas.vandaal@outlook.be>
2024-05-17 16:24:30 +0200

    fix: add backoffice context parceladdress count ef migration

---
+++ src/ParcelRegistry.Api.BackOffice.Abstractions/Migrations/20240517142336_AddCountOnParcelAddress.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParcelRegistry.Api.BackOffice.Abstractions.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountOnParcelAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                schema: "ParcelRegistryBackOffice",
+                table: "ParcelAddressRelation",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                schema: "ParcelRegistryBackOffice",
+                table: "ParcelAddressRelation");
+        }
+    }
+}
