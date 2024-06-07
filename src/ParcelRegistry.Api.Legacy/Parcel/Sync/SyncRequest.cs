0292b26c4db9aa5248052aff6b35ec44b5eaf085
Arne Dumarey <dumarey.arne@gmail.com>
2024-06-07 20:20:51 +0200

    feat: remove api legacy

--- src/ParcelRegistry.Api.Legacy/Parcel/Sync/SyncRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace ParcelRegistry.Api.Legacy.Parcel.Sync
-{
-    using MediatR;
-    using Microsoft.AspNetCore.Http;
-
-    public record SyncRequest(HttpRequest HttpRequest) : IRequest<string>;
-}
